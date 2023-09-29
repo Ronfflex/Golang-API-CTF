2986a234be6eb31ad5371e380cb447c0ccc304d4
Vincent Rainaud <aiglevisnky@gmail.com>
2023-09-29 12:02:06 +0200

    Logging all infos from a path using open port found

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"strconv"
 	"sync"
@@ -12,10 +14,12 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func init() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("No .env ? Source .env first ?")
-	}
+type ResponseDetails struct {
+	Status     string
+	StatusCode int
+	Headers    map[string][]string
+	Body       string
+	Error      string
 }
 
 func CheckPort(host string, port int, timeout time.Duration) bool {
@@ -55,31 +59,79 @@ func FindOpenPorts(host string, start, end int, timeout time.Duration) []int {
 	return openPorts
 }
 
+func FetchDetails(ip string, port int, path string) *ResponseDetails {
+	responseDetails := &ResponseDetails{}
+	url := fmt.Sprintf("http://%s:%d%s", ip, port, path)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		responseDetails.Error = err.Error()
+		return responseDetails
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		responseDetails.Error = err.Error()
+		return responseDetails
+	}
+
+	responseDetails.Status = resp.Status
+	responseDetails.Headers = resp.Header
+	responseDetails.Body = string(body)
+	return responseDetails
+}
+
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("No .env found")
+	}
+
 	ip := os.Getenv("IP")
 	startPortStr := os.Getenv("START_PORT")
 	endPortStr := os.Getenv("END_PORT")
 	timeoutMillisStr := os.Getenv("TIMEOUT")
 	timeoutMillis, err := strconv.Atoi(timeoutMillisStr)
+
 	if err != nil {
-		log.Fatalf("Error converting timeout to integer: %v", err)
+		log.Fatal("Error converting timeout to integer: ", err)
 	}
 	timeout := time.Duration(timeoutMillis) * time.Millisecond
 
 	startPort, err := strconv.Atoi(startPortStr)
 	if err != nil {
-		log.Fatalf("Error converting start port to integer: %v", err)
+		log.Fatal("Error converting start port to integer: ", err)
 	}
 
 	endPort, err := strconv.Atoi(endPortStr)
 	if err != nil {
-		log.Fatalf("Error converting end port to integer: %v", err)
+		log.Fatal("Error converting end port to integer: ", err)
 	}
 
+	paths := []string{"/ping", "/signup", "/check"}
+
 	openPorts := FindOpenPorts(ip, startPort, endPort, timeout)
-	if len(openPorts) > 0 {
-		fmt.Println("OPEN PORTS FOUND : ", openPorts)
-	} else {
-		fmt.Println("NO OPEN PORTS FOUND !???")
+	if len(openPorts) == 0 {
+		fmt.Println("Couldn't find any open ports in the specified range.")
+		return
+	}
+
+	for _, port := range openPorts {
+		for _, path := range paths {
+			fmt.Println("Fetching details for port :", port,", path", path)
+			details := FetchDetails(ip, port, path)
+
+			if details.Error != "" {
+				fmt.Println("Port,", port, "Path", path, ":\n Error :", details.Error)
+				continue
+			}
+
+			fmt.Println("Port", port,": Status :", details.Status)
+			fmt.Println("Port", port,": Headers :", details.Headers)
+			fmt.Println("Port", port,": Body :", details.Body)
+			fmt.Println("---------------------------------------------")
+		}
 	}
 }
